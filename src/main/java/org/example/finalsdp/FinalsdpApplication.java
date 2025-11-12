dd734cfb3517cf133e1b29259e0207d18c153ef8
aruwksc <240966@astanait.edu.kz>
2025-11-12 12:40:34 +0500

    first commit

---
+++ src/main/java/org/example/finalsdp/FinalsdpApplication.java
@@ -0,0 +1,13 @@
+package org.example.finalsdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalsdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalsdpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/finalsdp/FinalsdpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.finalsdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalsdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
